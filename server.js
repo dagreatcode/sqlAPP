6e8955a6a79a0f8f431c3b095a870ac33355fab0
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-19 19:28:35 -0400

    finished with the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require ("express");
+const e = require("express");
+const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
 
@@ -6,11 +7,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// API ROUTES
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
