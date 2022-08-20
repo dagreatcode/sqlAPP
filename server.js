273f49a0195954c0f4793cd6a0a8af909c7e4c99
Vincent Kendrick <dagreatcode@gmail.com>
2022-08-19 23:05:28 -0400

    fixing handlebars

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-const e = require("express");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// MIDDLEWEAR
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// VIEW ROUTES
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// app.get("/home", (req, res) => {
+//   res.render("home");
+// });
+// app.get("/about-us", (req, res) => {
+//     res.render("about-us");
+//   });
 
 // API ROUTES
 app.get("/api/config", (req, res) => {
